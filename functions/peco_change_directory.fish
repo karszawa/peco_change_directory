feac1b32f11a4af29fb375a242848219bbd71857
Hiroaki KARASAWA <karszawa@users.noreply.github.com>
2020-02-27 12:38:18 +0900

    chore: enable git vcs option

--- functions/peco_change_directory.fish
+++ functions/peco_change_directory.fish
@@ -16,8 +16,8 @@ function peco_change_directory
     echo $HOME/.config
     ls -ad */ | perl -pe "s#^#$PWD/#" | egrep -v "^$PWD/\." | head -n 5
     sort -r -t '|' -k 3 $Z_DATA | sed -e 's/\|.*//'
-    ghq list -p
-    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
+    ghq list -p --vcs git
+    ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
   end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
 end
 
